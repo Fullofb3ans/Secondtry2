3223e0706f78090c601ebecba96b573f72b94935
Developer4 <igorgazinskii6@gmail.com>
2022-09-30 15:46:20 +0400

    2

---
+++ src/CURSI/Secondtry.java
@@ -0,0 +1,94 @@
+package CURSI;
+
+public class Secondtry {
+
+
+    public static void main(String[] args) {
+           String b = "dab";
+        int a = 10;
+        int  c = 0;
+//        boolean b = true;
+        //  c = pravda(a, b);
+//        a = Switch(a);
+      //  While(a, b, c);
+//    d = first(a, b, c);
+//        if (c == true){
+//        System.out.println("Отрицательное");}
+//        else {System.out.println("положительное");}
+     //   int c = 0;
+//        int a = 500;
+//        boolean b = true;
+     chetire(a, b, c);
+
+    }
+
+
+
+//    private static int Switch(int a) {
+//
+//
+//
+//        switch (a) {
+//            case 11 -> {
+//                a--;
+//
+//            }
+//            case 10 -> {
+//                a++;
+//
+//            }
+//
+//        }
+//
+//        return a;
+//    }
+//
+//    private static void While(int a , int b){
+//
+//        while (a < b)  {
+//         a++;
+//            System.out.println(a);}
+//        }
+//
+//    }
+
+
+//    public static void afor() {
+//        for (int a=1; a<5; a++)
+//        System.out.println(a);
+//    }
+
+//    public static boolean first(int a, int b, boolean c) {
+//        if ((a+b) < 20 && (a+b) > 10){
+//            c = true;
+//        }
+//        else c = false;
+//        return c;
+//    }
+
+//    public static boolean pravda(int a, boolean b) {
+//        if (a > 0) { b = false; }
+//        else b = true;
+//        return b;
+//
+//    }
+
+    public static void chetire(int a, String b, int c) {
+
+        while (a > c) {
+            System.out.println(b);
+            a--;
+
+        }
+
+
+
+    }
+
+//    public static boolean pyat(boolean b, int a){
+//        if (((a%4 == 0) && (a % 100 > 0 )) || (a % 400 == 0 )) {b = true;}
+//        else {b= false;}
+//        return b;
+//    }
+
+}
---
+++ src/CURSI/Third.java
@@ -0,0 +1,96 @@
+package CURSI;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+
+        //ПЯТОЕ____________________________________________________________________
+//        int a = 7;
+//        int b = 3;
+//        int[] c = met(a, b);
+//
+//        System.out.println(Arrays.toString(c));
+//    }
+//____________________________________________________________________
+
+//        //Шестое____________________________________________________________________
+//
+//
+//        int[] arr = {5, 2, 1, 4};int min = arr[0]; int max = arr[0];
+//        for (int i = 0; i<arr.length; i++) {
+//
+//
+//
+//            if(arr[i]<min) {min =arr[i];}
+//            if(arr[i]>max) {max =arr[i];}
+//
+//                  }
+//
+//        System.out.println(min);
+//        System.out.println(max);
+//
+//    }
+//}
+//Седьмое____________________________________________________________________
+
+
+        //ПЕРВОЕ ЗАДАНИЕ
+//        __________________________________________________________________
+//        //int [] array = new int[5];
+//       int [] arr = {1, 0, 1, 0, 1};
+//       for  (int i = 0; i< arr.length; i++)
+//
+//       {if (arr[i] == 0) {arr[i] = 1;}
+//       else {arr[i] = 0;}
+//
+//        }
+//        System.out.println(Arrays.toString(arr));
+//
+        //ВТОРОЕ ЗАДАНИЕ
+//        ____________________________________________________________________
+//int [] arr = new int[100];
+//for (int i = 0; i< arr.length; i++) {arr[i] = i + 1;}
+//
+//    System.out.println(Arrays.toString(arr));
+
+        //Третье ЗАДАНИЕ
+//        ____________________________________________________________________
+//
+//int [] array = new int [] {1, 5, 3, 2, 11 ,4, 5, 2, 4, 8, 9, 1};
+//for (int i = 0; i< array.length; i++)
+//    if (array[i]< 6) { array[i] = array[i] * 2;}
+//System.out.println(Arrays.toString(array));
+// Четвертое задание
+//         _________________________________________________________________________________
+
+//        int [][] array = new int [7][7];
+//        for (int i=0; i<array.length; i++){
+//         for (int j=0; j<array[i].length; j++) {
+//              {if ((i == j) || (j + 1 == array.length - i  )){array[i][j] = 1;}
+////             int check[][] =  array;
+//             //   {if (j == array[i].length - j){array[i][j] = 1;}
+//             System.out.print(array[i][j] + " ");
+//
+//         }
+//
+//
+//
+//
+//}
+//           System.out.println();
+//    }
+//}}
+// ПЯТОЕ ЗАДАНИЕ
+        //____________________________________________________________________
+//public static int[] met(int a, int b) {
+//
+//    int array[] = new int [a];
+//    for (int i = 0; i<a; i++) { array[i] = b;}
+//
+//    return array;
+//}
+//____________________________________________________________________
+
+    }}
